c72ad104dcaf436aefa4dd6eb9adc367242baaeb
Erick Malta <erickmalta100@gmail.com>
2020-06-10 12:29:49 -0300

    Added a test for users

--- src/app.js
+++ src/app.js
@@ -4,4 +4,10 @@ app.get("/", (req, res) => {
   res.status(200).send();
 });
 
+app.get("/users", (req, res) => {
+  const users = [{ name: "John Doe", mail: "john@mail.com" }];
+
+  res.status(200).json(users);
+});
+
 module.exports = app;
